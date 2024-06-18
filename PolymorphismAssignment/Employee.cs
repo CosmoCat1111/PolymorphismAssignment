7705a41bb50006e033363f0167bc371b39c679fd
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 21:11:37 -0700

    Add project files.

---
+++ PolymorphismAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Employee : IQuittable //calls interface
+    {
+        public void Quit() //implementing method
+        {
+            Console.WriteLine("Employee quits job!");
+        }
+    }
+}
---
+++ PolymorphismAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(); //creates instance of employee
+            IQuittable quittable = emp; //polymorphism creates object of iquittable
+            quittable.Quit(); //call the method
+            Console.ReadLine();
+
+        }
+    }
+}
